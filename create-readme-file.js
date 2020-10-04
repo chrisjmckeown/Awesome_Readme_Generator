8a25b4139b5266406f796dc38dbd6df8252d50c7
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-10-04 11:21:41 +1100

    dynamic user inputs creating readme file

--- create-readme-file.js
+++ create-readme-file.js
@@ -1,42 +1,44 @@
-function createReadme(userInput) {
-    let readme = `# ${userInput.reponame}
-[![license](https://img.shields.io/github/license/${userInput.username}/${userInput.reponame}.svg?style=flat-square)](https://github.com/${userInput.username}/${userInput.reponame}/blob/master/LICENSE)
-
-${userInput.Description}
-
-## Table of Contents
-* [Installation](#Installation)
-* [Usage](#Usage)
-* [License](#License)
-* [Contributing](#Contributing)
-* [Tests](#Tests)
-* [Questions](#Questions)
-
-## Installation
-${userInput.Installation}
-
-## Usage
-${userInput.Usage}
-
-## License
-${userInput.License}
-
-[![license](https://img.shields.io/github/license/${userInput.user}/${userInput.reponame}.svg?style=flat-square)](https://github.com/${userInput.user}/${userInput.reponame}/blob/master/LICENSE)
-
-## Contributing
-${userInput.Contributing}
-
-[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)
-
-## Tests
-${userInput.Tests}
-
-## Questions
-* Follow me at: <a href="https://github.com/${userInput.username}" target="_blank">https://github.com/${userInput.username}</a>
-* Please email with any question at: ${userInput.email}
-
-© 2019 ${userInput.username}`;
+function createReadmeBody(userInput) {
+    // build the body to match the user requirements
+    let readme = "";
+    if (userInput.Installation) readme += `\n\n## Installation\n${userInput.Installation}`;
+    if (userInput.Usage) readme += `\n\n## Usage\n${userInput.Usage}`;
+    if (userInput.License) readme += `\n\n## License\n${userInput.License}\n[![license](https://img.shields.io/github/license/${userInput.user}/${userInput.reponame}.svg?style=flat-square)](https://github.com/${userInput.user}/${userInput.reponame}/blob/master/LICENSE)`;
+    if (userInput.Contributing) readme += `\n\n## Contributing\n${userInput.Contributing}\n[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)`;
+    if (userInput.Tests) readme += `\n\n## Tests\n${userInput.Tests}`;
     return readme;
+}
+
+function createReadmeHeader(userInput) {
+    // build the title, license and description
+    let readme = `# ${userInput.reponame}
+    \n[![license](https://img.shields.io/github/license/${userInput.username}/${userInput.reponame}.svg?style=flat-square)](https://github.com/${userInput.username}/${userInput.reponame}/blob/master/LICENSE)
+    \n${userInput.Description}
+    \n## Table of Contents`;
+    // build the table of contents to match the use input.
+    if (userInput.Installation) readme += `\n* [Installation](#Installation)`;
+    if (userInput.Usage) readme += `\n* [Usage](#Usage)`;
+    if (userInput.License) readme += `\n* [License](#License)`;
+    if (userInput.Contributing) readme += `\n* [Contributing](#Contributing)`;
+    if (userInput.Tests) readme += `\n* [Tests](#Tests)`;
+    readme += `\n* [Questions](#Questions)`
+    return readme;
+}
+
+function createReadmeFooter(userInput) {
+    const year = new Date().getFullYear();
+    const readme = `\n\n## Questions
+    \n* Follow me at: <a href="https://github.com/${userInput.username}" target="_blank">https://github.com/${userInput.username}</a>
+    \n* Please email with any question at: ${userInput.email}
+    \n© ${year} ${userInput.username}`;
+    return readme;
+}
+
+function createReadme(userInput) {
+    const header = createReadmeHeader(userInput);
+    const body = createReadmeBody(userInput);
+    const footer = createReadmeFooter(userInput);
+    return header + body + footer;
 }
 
 module.exports = {
@@ -44,9 +46,3 @@ module.exports = {
         return createReadme(userInput);
     },
 };
-
-// ;
-//     if (userInput.contributingBadge === "yes") {
-//         readme += `\n[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)`;
-//     }
-//     readme += 
--- readme.md
+++ readme.md
@@ -1,8 +1,9 @@
 # Awesome_Readme_Generator
+    
 [![license](https://img.shields.io/github/license/chrisjmckeown/Awesome_Readme_Generator.svg?style=flat-square)](https://github.com/chrisjmckeown/Awesome_Readme_Generator/blob/master/LICENSE)
-
+    
 d
-
+    
 ## Table of Contents
 * [Installation](#Installation)
 * [Usage](#Usage)
@@ -15,23 +16,22 @@ d
 i
 
 ## Usage
-undefined
+u
 
 ## License
-undefined
-
+l
 [![license](https://img.shields.io/github/license/undefined/Awesome_Readme_Generator.svg?style=flat-square)](https://github.com/undefined/Awesome_Readme_Generator/blob/master/LICENSE)
 
 ## Contributing
-undefined
-
+c
 [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)
 
 ## Tests
-undefined
-
+t
 ## Questions
+    
 * Follow me at: <a href="https://github.com/chrisjmckeown" target="_blank">https://github.com/chrisjmckeown</a>
+    
 * Please email with any question at: c
-
-© 2019 chrisjmckeown
+    
+© 2020 chrisjmckeown
